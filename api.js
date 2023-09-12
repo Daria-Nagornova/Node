5fd3948c9f4454f2ccf366a70839a2038a4098a2
Daria Nagornova <daria50476@gmail.com>
2023-09-12 17:32:22 +0300

    Запрос, запуск локального сервера

--- api.js
+++ api.js
@@ -2,17 +2,14 @@ const axios = require('axios')
 const fs = require('fs');
 const path = require('path');
 
- function requestData() {
-     (async () => {
-         const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+async function requestData() {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
 
          fs.writeFileSync(path.resolve(__dirname, 'new_file.json'), JSON.stringify(data), 'utf-8', (err) => {
              if (err) {
                  console.log('Ошибка записи в файл');
              }
          })
-
-     })();
  }
 
 module.exports = {
