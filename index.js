1602f84973715d1eef80a2ec69f2bf7a31ec6269
Daria Nagornova <daria50476@gmail.com>
2023-09-12 11:28:03 +0300

    Запрос, запуск локального сервера

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const M = 20;
-const N = 30;
+const http = require('http');
+const api = require('./api');
 
-for(let i = M; i <= N; i++) {
-    console.log(i);
-}
+const server = http.createServer((request, response) => {
 
+    if(request.url === '/request') api.requestData();
+
+    response.end();
+
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://127.0.0.1:3000/');
+});
